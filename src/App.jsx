eb60c4547ba823d343ff00f50d070e089d5c5f40
emetreve <emetreve@gmail.com>
2023-05-08 20:49:56 +0400

    style: add custom font

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from '~/assets/react.svg';
 function App() {
   return (
     <>
-      <h1 className='text-sky-400 text-7xl font-bold underline font-BpgArial'>
+      <h1 className='text-sky-400 text-7xl font-bold font-BpgArial'>
         გამარჯობა
       </h1>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@
 const config = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    fontFamily: {
-      BpgArial: ['BpgArial', 'sans-serif'],
+    extend: {
+      fontFamily: {
+        BpgArial: ['BpgArial', 'sans-serif'],
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
