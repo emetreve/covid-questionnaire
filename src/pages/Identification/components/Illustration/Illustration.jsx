ce20605b67aaf96b4d18eed382d2b7880d50227f
emetreve <emetreve@gmail.com>
2023-05-09 21:09:05 +0400

    style: animate rectangle on eyes

--- src/pages/Identification/components/Illustration/Illustration.jsx
+++ src/pages/Identification/components/Illustration/Illustration.jsx
@@ -1,11 +1,12 @@
 function Illustration() {
   return (
-    <div>
+    <div className='relative'>
       <img
         src='/assets/star-eyed-ones.png'
         alt='two people with starry eyes'
-        className='scale-[165%] pr-28 pt-14'
+        className='scale-[165%] pr-28 pt-14 relative z-10'
       />
+      <div className='absolute animate-rect bg-yellow-eyes' />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,35 @@ const config = {
         '75%': { top: '-6rem', opacity: 0.9 },
         '100%': { top: 0, opacity: 1 },
       },
+      rect: {
+        '0%': {
+          top: '10rem',
+          left: '-4.375rem',
+          height: '5rem',
+          width: '2.1875rem',
+          backgroundColor: 'rgba(214, 209, 110, 0.2)',
+        },
+        '25%': {
+          height: '5rem',
+          backgroundColor: 'rgba(214, 209, 110, 0.4)',
+        },
+        '75%': {
+          height: '5rem',
+          backgroundColor: 'rgba(214, 209, 110, 0.8)',
+        },
+        '100%': {
+          top: '8.75rem',
+          left: '-3.75rem',
+          width: '38.75rem',
+          height: '5rem',
+          backgroundColor: '#D6D16E',
+        },
+      },
     },
     animation: {
       'dissolve-logo': 'dissolveLandingLogo 0.8s forwards',
       'text-drop': 'textDrop 1s ease-out 700ms forwards',
+      rect: 'rect 0.9s forwards',
     },
     fontFamily: {
       BpgArial: ['BpgArial', 'sans-serif'],
@@ -27,6 +52,7 @@ const config = {
       colors: {
         'granular-white': '#FAF9F6',
         black: '#232323',
+        'yellow-eyes': '#D6D16E',
       },
     },
   },
