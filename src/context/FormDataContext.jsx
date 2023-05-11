ee443fa611b88004521078aeaa3942298f25c18c
emetreve <emetreve@gmail.com>
2023-05-11 18:15:58 +0400

    refactor: remove comment

--- src/context/FormDataContext.jsx
+++ src/context/FormDataContext.jsx
@@ -3,11 +3,7 @@ import { createContext, useState } from 'react';
 const FormDataContext = createContext();
 
 function FormDataProvider({ children }) {
-  const [formData, setFormData] = useState({
-    // first_name: localStorage.getItem('first_name') || '',
-    // last_name: localStorage.getItem('last_name') || '',
-    // email: localStorage.getItem('email') || '',
-  });
+  const [formData, setFormData] = useState({});
 
   const updateFormData = (data) => {
     setFormData((prevState) => ({ ...prevState, data }));
