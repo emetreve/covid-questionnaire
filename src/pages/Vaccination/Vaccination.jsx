887b18efb5332e80bf2165d000d5479ef78c13a6
emetreve <emetreve@gmail.com>
2023-05-11 19:18:12 +0400

    feat(vaccin): add header

--- src/pages/Vaccination/Vaccination.jsx
+++ src/pages/Vaccination/Vaccination.jsx
@@ -1,8 +1,11 @@
+import { FormTracker } from '@/components';
+
 function Vaccinaton() {
   return (
-    <>
+    <div className='px-44 h-screen uppercase bg-granular-white relative'>
+      <FormTracker progress={3} />
       <h1>Vaccination page over here</h1>
-    </>
+    </div>
   );
 }
 export default Vaccinaton;
