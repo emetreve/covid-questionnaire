719ad602f65571094ce742a67f7b8282f994cd24
emetreve <emetreve@gmail.com>
2023-05-11 19:16:52 +0400

    feat: setup and include vaccination page in routes library

---
+++ src/pages/Vaccination/Vaccination.jsx
@@ -0,0 +1,8 @@
+function Vaccinaton() {
+  return (
+    <>
+      <h1>Vaccination page over here</h1>
+    </>
+  );
+}
+export default Vaccinaton;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export * from './Landing';
 export * from './Identification';
 export * from './Covid';
+export * from './Vaccination';
