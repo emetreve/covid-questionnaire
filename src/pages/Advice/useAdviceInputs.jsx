441846d92e80864bef9d4e3fd4b6173b62c55e1f
emetreve <emetreve@gmail.com>
2023-05-13 16:22:13 +0400

    feat: conditionally handle antibodies data before submitting to server

--- src/pages/Advice/useAdviceInputs.jsx
+++ src/pages/Advice/useAdviceInputs.jsx
@@ -5,7 +5,7 @@ import { FormDataContext } from '@/context';
 import { ROUTES } from '@/config';
 
 function useAdviceInputs() {
-  const { updateFormData } = useContext(FormDataContext);
+  const { updateFormData, formData } = useContext(FormDataContext);
   const navigate = useNavigate();
 
   const { register, handleSubmit } = useForm({
@@ -24,7 +24,34 @@ function useAdviceInputs() {
   const onSubmit = (data) => {
     updateFormData(data);
     navigate(ROUTES.THANK_YOU);
+    const contextData = { ...formData };
+    Object.entries(contextData).forEach(([key, value]) => {
+      if (value === '') {
+        delete contextData[key];
+      }
+      if (key === 'antibodies') {
+        const number = contextData[key].number !== '';
+        const test_date = contextData[key].test_date !== '';
+        if (!number) {
+          delete contextData[key].number;
+        } else {
+          contextData[key].number = Number(contextData[key].number);
+        }
+        if (!test_date) {
+          delete contextData[key].test_date;
+        }
+        if (!number && !test_date) {
+          delete contextData[key];
+        }
+      }
+    });
+    console.log(222, contextData);
   };
+
+  //had_antibody_test
+  //had_vaccine
+
+  //number_of_days_from_office
 
   return {
     handleSubmit,
