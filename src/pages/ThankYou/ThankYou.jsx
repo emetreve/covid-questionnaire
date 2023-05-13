cef05e2d875f6b88da019eebe78e42a6bba5a994
emetreve <emetreve@gmail.com>
2023-05-13 17:15:33 +0400

    refactor: remove extra comments

--- src/pages/ThankYou/ThankYou.jsx
+++ src/pages/ThankYou/ThankYou.jsx
@@ -1,11 +1,4 @@
-import { useContext } from 'react';
-import { FormDataContext } from '@/context';
-
 function ThankYou() {
-  const { formData } = useContext(FormDataContext);
-
-  // console.log(222, formData);
-
   return <div>THANK YOU</div>;
 }
 
