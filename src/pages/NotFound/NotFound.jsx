066fe135c0c5792847275ba26b35d84983b7f63e
emetreve <emetreve@gmail.com>
2023-05-15 20:58:57 +0400

    fix: use Georgian on 404 page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,7 @@
 function NotFound() {
   return (
     <div className='flex justify-center h-screen items-center text-3xl bg-gray text-white'>
-      <h1>This page does not exist</h1>
+      <h1>გვერდი არ არსებობს</h1>
     </div>
   );
 }
