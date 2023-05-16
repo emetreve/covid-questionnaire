aee3bf8c665f944db6fed1e21dedeb1f550c1b2c
emetreve <emetreve@gmail.com>
2023-05-16 19:40:43 +0400

    refactor: move routes library code to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { RoutesLibrary } from '@/config';
+import { Routes, Route } from 'react-router-dom';
+import { ROUTES } from '@/config';
+import {
+  Landing,
+  Identification,
+  Covid,
+  Vaccination,
+  Advice,
+  NotFound,
+} from '@/pages';
 
 function App() {
   return (
-    <>
-      <RoutesLibrary />
-    </>
+    <Routes>
+      <Route path={ROUTES.LANDING} element={<Landing />} />
+      <Route path={ROUTES.IDENTIFICATION} element={<Identification />} />
+      <Route path={ROUTES.COVID} element={<Covid />} />
+      <Route path={ROUTES.VACCINATION} element={<Vaccination />} />
+      <Route path={ROUTES.ADVICE} element={<Advice />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   );
 }
 
