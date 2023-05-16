42a1690d42a150f98a81fb06c696dadf44578715
emetreve <emetreve@gmail.com>
2023-05-17 02:18:35 +0400

    refactor: remove extra code

--- src/pages/Advice/components/Modal/Modal.jsx
+++ src/pages/Advice/components/Modal/Modal.jsx
@@ -16,11 +16,7 @@ function Modal() {
           className='h-8 animate-sparkle-bottom absolute z-50'
         />
       </div>
-      {/* <img
-        src='/assets/heart.png'
-        alt='heard'
-        className='animate-heart-zoom absolute z-50 h-[5rem] w-[10rem]'
-      /> */}
+
       <svg
         id='heart-svg'
         width='196'
@@ -32,7 +28,6 @@ function Modal() {
         <path
           id='heart-path'
           d='M48.5005 12C75.7005 9.6 94.5005 28.3333 100.501 38C110.501 1.5 143.5 0.5 161.5 0.5C179.5 0.5 203.5 22 192.5 69C183.7 106.6 144.5 153.667 126 172.5C87.6668 153.5 9.30051 107.3 2.50051 74.5C-5.99949 33.5 14.5005 15 48.5005 12Z'
-          // fill='#F39494'
           className='animate-heart-color'
         />
       </svg>
