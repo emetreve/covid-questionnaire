b4378558af98b06b2089a1462c608bee3e0204ca
emetreve <emetreve@gmail.com>
2023-05-17 13:00:16 +0400

    refactor: remove comment

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,11 +5,6 @@ import { LogoRound } from '@/components';
 function Landing() {
   return (
     <div className='bg-granular-white flex flex-col justify-center items-center h-screen uppercase'>
-      {/* <img
-        src='/assets/logo.svg'
-        alt='Redberry rocket'
-        className='mb-[5rem] animate-dissolve-logo h-28'
-      /> */}
       <LogoRound classes='mb-[5rem] animate-dissolve-logo h-28' />
       <div className='text-center h-25 relative w-60 overflow-y-hidden'>
         <Link
